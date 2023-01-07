a27a46c7a5422fc9e287df2ce96c2b9b2f513e10
eulseon <eulseon2004@gmail.com>
2023-01-08 00:00:22 +0900

    first commit

---
+++ arfarmPrSite/src/main/java/com/example/arfarmPrSite/ArfarmPrSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.arfarmPrSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArfarmPrSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArfarmPrSiteApplication.class, args);
+	}
+
+}
